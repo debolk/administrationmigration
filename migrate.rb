d89b49a9b30bafc931f6ee2f18a8acf19e3a9ee4
Jakob Buis <jpabuis@gmail.com>
2013-07-17 13:50:27 +0200

    could work

--- migrate.rb
+++ migrate.rb
@@ -1,10 +1,110 @@
-# Load gems
+# Guard to prevent accidental execution
+exit
+
+# Load gems needed to talk to blip and operculum
 require 'json'
 require 'net/http'
 
-# Open the needed files
-ldap = File.open('output.ldif', 'w')
-mysql = File.open('output.sql', 'w')
+#
+# Some useful functions
+#
+
+# POST to a URL with a payload, returning the result
+def post(url, payload)
+  url = URI.parse(url)
+  http = Net::HTTP.new(url.host, url.port)
+  http.use_ssl = true
+  request = Net::HTTP::Post.new(url.path+'?access_token=verysecret')
+  request.content_type = 'application/json'
+  request.body = JSON.generate(payload)
+  response = http.start {|http| http.request(request) }
+  JSON.parse(response.body)
+end
+
+# PUT to a URL with a payload, returning the result
+def put(url, payload)
+  url = URI.parse(url)
+  http = Net::HTTP.new(url.host, url.port)
+  http.use_ssl = true
+  request = Net::HTTP::Put.new(url.path+'?access_token=verysecret')
+  request.content_type = 'application/json'
+  request.body = JSON.generate(payload)
+  response = http.start {|http| http.request(request) }
+  JSON.parse(response.body)
+end
+
+# PATCH to a URL with a payload, returning the result
+def patch(url, payload)
+  url = URI.parse(url)
+  http = Net::HTTP.new(url.host, url.port)
+  http.use_ssl = true
+  request = Net::HTTP::Patch.new(url.path+'?access_token=verysecret')
+  request.content_type = 'application/json'
+  request.body = JSON.generate(payload)
+  response = http.start {|http| http.request(request) }
+  JSON.parse(response.body)
+end
+
+# Creates a new user
+def create(params)
+  # Send payload to blip
+  blip = post('https://people.i.bolkhuis.nl/persons', {
+    initials: params[4],
+    firstname: params[5],
+    lastname: [params[6], params[7]].reject{|e|e.empty?}.join(' '),
+    email: params[3],
+    gender: params[9] == 'Man' ? 'M' : 'F',
+    phone: params[14],
+    mobile: params[2],
+    phone_parents: params[24],
+    address: [params[10], params[11], params[12]].join(' '),
+    dateofbirth: params[15],
+  })
+
+  # Grab uid
+  uid = blip['uid']
+  
+  # Send payload to operculum
+  put("https://operculum.i.bolkhuis.nl/persons/#{uid}", {
+    nickname: params[8],
+    study: params[16],
+    alive: !params[17].empty?,
+    inauguration: params[25],
+    resignation_letter: params[26],
+    resignation: params[27],
+  })
+end
+
+# Updates an existing user
+def update(uid, params)
+  # Send payload to blip
+  put("https://people.i.bolkhuis.nl/persons/#{uid}", {
+    initials: params[4],
+    email: params[3],
+    gender: params[9] == 'Man' ? 'M' : 'F',
+    phone: params[14],
+    mobile: params[2],
+    phone_parents: params[24],
+    address: [params[10], params[11], params[12]].join(' '),
+    dateofbirth: params[15],
+  })
+
+  # Send payload to operculum
+  put("https://operculum.i.bolkhuis.nl/persons/#{uid}", {
+    nickname: params[8],
+    study: params[16],
+    alive: !params[17].empty?,
+    inauguration: params[25],
+    resignation_letter: params[26],
+    resignation: params[27],
+  })
+end
+
+#
+# The actual processing
+#
+
+# Open needed files
 problems = File.open('problems.log', 'w')
 
 # Open the blip API index /persons
@@ -23,6 +123,14 @@ IO.foreach "data.csv" do |line|
 
   # Read the input
   input = line.split(';')
+
+  # Remove parenthesis
+  input.each do |e|
+    if e.length > 1
+      e[0] = ''
+      e[-1] = ''
+    end
+  end
 
   # Determine what to do based on the status of the member
   case input[1].downcase
@@ -43,55 +151,23 @@ IO.foreach "data.csv" do |line|
   # Find if member exists
   found = false
   count = 0
+  uid = nil
   blip.each do |entry|
-    count += 1 if '"'+(entry['name'].downcase)+'"' === input[0].downcase
+    if '"'+(entry['name'].downcase)+'"' === input[0].downcase
+      count += 1 
+      uid = entry['uid']
+    end
   end
 
   # Determine what to do
   if count === 0
     # Create new person
-    ldap.write "#{index}, #{input[0]}\n"
+    create(input)
   elsif count === 1
     # Update existing person
-    mysql.write "#{index}, #{input[0]}\n"
+    update(uid, input)
   else
     # Log as a problematic case with rule number and line
     problems.write "#{index}, #{input[0]}\n"
   end
 end
-# zoek hun volledige naam
-# if count == 1 {
-# update die persoon
-# }
-# elseif count === 0 {
-# maak nieuwe persoon
-# else {
-# log als probleemgeval met (regelnummer en naam)
-# }
-
-  # # Construct new entries
-  # ldap_data = {
-  #   mobile: input[2],
-  #   email: input[3],
-  #   initials: input[4],
-  #   givenname: input[5],
-  #   sn: [input[6], input[7]].reject{|e|e.empty?}.join(' '),
-  #   gender: input[9] == 'Man' ? 'M' : 'F',
-  #   address: [input[10], input[11], input[12]].join(' '),
-  #   phone: input[14],
-  #   birthdate: input[15],
-  # }
-  # mysql_data = {
-  #   study: input[16],
-  #   alive: !input[17].empty?,
-  #   phone_parents: input[24],
-  #   inauguration: input[25],
-  #   resignation_letter: input[26],
-  #   resignation: input[27],    
-  # }
-
-
-
-  # # Write to output
-  # ldap.write ldap_data
-  # mysql.write mysql_data
